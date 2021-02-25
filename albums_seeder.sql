6412d5ef49b83d42f8b0110359072890a70efe52
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-02-25 10:09:35 -0600

    feat: Made seeder and updated select

---
+++ albums_seeder.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+insert into albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop'),
+    ('AC/DC', 'Back in Black', 1980, 29.4, 'Hard Rock'),
+    ('Meat Loaf', 'Bat out of Hell', 1977, 21.7, 'Hard Rock'),
+    ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.4, 'Progressive Rock'),
+       ('Whitney Houston', 'The bodyguard', 1992, 28.4, 'R and B'),
+    ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 41.2, 'Country Rock'),
+       ('Bee Gees', 'Saturday Night Fever', 1977, 21.6, 'Disco'),
+       ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock'),
+       ('Shania Twain', 'Come on over', 1997, 29.6, 'Country');
--- select_exercises.sql
+++ select_exercises.sql
@@ -2,12 +2,12 @@ USE codeup_test_db;
 
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
-SELECT * FROM albums WHERE name = 'Lonely Hearts club';
+SELECT release_date FROM albums WHERE name = 'Lonely Hearts club';
 
-SELECT * FROM albums WHERE name = 'Nevermind';
+SELECT genre FROM albums WHERE name = 'Nevermind';
 
-SELECT * FROM albums WHERE release_date = 1990 - 2000;
+SELECT name FROM albums WHERE release_date > 1990 && release_date < 2000;
 
-SELECT * FROM albums WHERE sales < 20,000,000;
+SELECT * FROM albums WHERE sales < 20.0;
 
 SELECT * FROM albums WHERE genre = 'Rock';
