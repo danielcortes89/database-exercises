ac79fabd37a150256a83728d73a1b3fb7056466f
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-02-26 15:51:33 -0600

    feat: Aliases success

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,20 +1,31 @@
 USE employees;
 
-SELECT DISTINCT title, COUNT(*)
+SELECT DISTINCT title, COUNT(title)
     FROM employess
     GROUP BY title;
 
-SELECT last_name, COUNT(*)
+SELECT last_name, COUNT(last_name)
     FROM employees
     WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
     GROUP BY last_name;
 
-SELECT last_name, COUNT(*)
+SELECT last_name, COUNT(last_name)
     FROM employees
     WHERE last_name LIKE 'E%' AND last_name LIKE '%e'
     AND first_name LIKE 'E%' AND first_name LIKE '%e'
     GROUP BY last_name;
 
-SELECT * FROM employees
+SELECT last_name
+    FROM employees
     WHERE last_name LIKE '%q%'
     AND last_name NOT LIKE '%qu%';
+
+SELECT last_name, COUNT(last_name)
+    FROM employees
+    WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%'
+    ORDER BY last_name;
+
+SELECT COUNT(gender), gender FROM employees
+    WHERE (first_name = 'Irena' OR 'Vidya' OR 'Maya')
+    AND gender = 'M' ORDER BY first_name DESC, last_name DESC;
